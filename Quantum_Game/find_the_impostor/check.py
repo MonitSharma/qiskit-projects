81839df4b3db1506725f1ed0090e74991d3ba187
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-07-10 21:41:48 +0530

    Add files via upload

---
+++ Quantum_Game/find_the_impostor/check.py
@@ -0,0 +1,18 @@
+result = []
+gate = input("what do you want? ")
+result.append(gate)
+
+if gate == 'r':
+    angle = input("ENter Phaser ")
+    result.append(angle)
+
+elif gate == 'cx':
+    control = input("Enter Control ")
+    result.append(control)
+
+else:
+    result.append("b")
+
+target = input("Enter ")
+result.append(target)
+
